57b7d9527f38b0031f6aceeed9b125a4b9a6b80f
ASpeedyNiNjA <106184909+ASpeedyNiNjA@users.noreply.github.com>
2023-12-04 13:01:34 -0500

    Add files via upload

---
+++ assignment-final/header-footer.js
@@ -0,0 +1,12 @@
+const header = `
+<nav>
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li><a href="company.html">Company</a></li>
+		<li><a href="product.html">Playstation</a></li>
+		<li><a href="contact.html">Contact</a></li>
+	</ul>
+</nav>
+`
+
+document.getElementById("js-head").innerHTML = header;
